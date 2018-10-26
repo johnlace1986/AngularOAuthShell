e111eac576ae297efcae2d3b583e28aa4f6dcf82
John Lace <lace.john@gmail.com>
2018-10-26 09:22:30 +0100

    Moved routing to app module file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from "@angular/http";
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PortalComponent } from './portal/portal.component';
 import { AuthModule } from './auth/auth.module';
+import { RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth-guard';
+import { AuthCallbackComponent } from './auth/auth-callback/auth-callback.component';
 
 
 @NgModule({
@@ -16,8 +18,12 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     HttpModule,
     BrowserModule,
-    AppRoutingModule,
-    AuthModule
+    AuthModule,
+    RouterModule.forRoot([
+      { path: 'portal', component: PortalComponent, canActivate: [AuthGuard] },
+      { path: 'auth-callback', component: AuthCallbackComponent },
+      { path: '**', redirectTo: 'portal' }
+    ])
   ],
   bootstrap: [AppComponent]
 })
